7485f5f6394e44dc0a7f75219fc35a3c6dd516b7
riyaraa <rinosatyaputra.id@gmail.com>
2022-01-14 07:41:54 +0700

    initial commit

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+function App() {
+	return (
+		<>
+			<Router>
+				<Routes>{/* <Route exact path="/" element={} /> */}</Routes>
+			</Router>
+		</>
+	);
+}
+
+export default App;
